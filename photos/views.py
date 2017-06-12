4a6c9d52062ae5c90b707eae4d0c1c8b23590684
DrakeYang <drake.the.programmer@gamil.com>
2017-06-12 17:57:31 +0900

    completed step 4 and uploaded

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Photo
+from django.shortcuts import get_object_or_404
 
 def hello(request):
     return HttpResponse('안녕하세요!')
 
-def detail(request, pk):
-    msg = '{}번 사진 보여줄게요.'.format(pk)
-    return HttpResponse(msg)
+def detail(request, pk, hidden=False):
+    photo = get_object_or_404(Photo, pk=pk)
+    if hidden is True:
+        # todo: 뭔가 은밀한 작업을 합시다
+        pass
+    messages = (
+        '<p>{pk}번 사진 보여줄게요</p>'.format(pk=photo.pk),
+        '<p>주소는 {url}</p>'.format(url=photo.image.url),
+        '<p><img src="{url}" /></p>'.format(url=photo.image.url),
+    )
+
+    return HttpResponse('\n'.join(messages))
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from photos.views import hello
-from photos.views import detail
+from photos.views import hello,detail
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),
-    url(r'^photos/(?P<pk>[0-9]+)/$',
-    	detail, name='detail'),  
+    url(r'^photos/(?P<pk>[0-9]+)/$',detail, name='detail'),
+    url(r'^hidden-photos/(?P<pk>[0-9]+)$', detail, kwargs={'hidden':True}),
 ]
+urlpatterns += static('upload_files', document_root=settings.MEDIA_ROOT)
